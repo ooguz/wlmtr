204c027479cf99e207d94fdab2cae88700b42674
Özcan Oğuz <ozcan@oyd.org.tr>
2025-09-26 20:09:57 +0300

    config: point wikimedia oauth/api base to OAuth2 rest endpoints

--- config/services.php
+++ config/services.php
@@ -39,8 +39,8 @@ return [
         'client_id' => env('WIKIMEDIA_CLIENT_ID'),
         'client_secret' => env('WIKIMEDIA_CLIENT_SECRET'),
         'redirect' => env('WIKIMEDIA_REDIRECT_URI'),
-        'oauth_url' => env('WIKIMEDIA_OAUTH_URL', 'https://meta.wikimedia.org/w/index.php'),
-        'api_url' => env('WIKIMEDIA_API_URL', 'https://meta.wikimedia.org/w/api.php'),
+        'oauth_url' => env('WIKIMEDIA_OAUTH_URL', 'https://meta.wikimedia.org/w/rest.php/oauth2'),
+        'api_url' => env('WIKIMEDIA_API_URL', 'https://meta.wikimedia.org/w/rest.php/oauth2'),
     ],
 
 ];
