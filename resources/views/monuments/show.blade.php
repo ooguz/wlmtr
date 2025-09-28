9c7e9304a27b8b126ea518b9af4331b009b09bf0
Özcan Oğuz <ozcan@oyd.org.tr>
2025-09-29 01:39:01 +0300

    ui: Change labels

--- resources/views/monuments/show.blade.php
+++ resources/views/monuments/show.blade.php
@@ -358,7 +358,7 @@
                             <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                             </svg>
-                            Wikipedia (TR)
+                            Vikipedi (Türkçe)
                         </a>
                     @endif
                     
@@ -369,7 +369,7 @@
                             <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                             </svg>
-                            Wikipedia (EN)
+                            Wikipedia (English)
                         </a>
                     @endif
                     
