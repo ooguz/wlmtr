f60e5ca36fc41b948a07e281973d235be9761e68
Özcan Oğuz <ozcan@oyd.org.tr>
2025-10-01 01:48:03 +0300

    hotfix: Commons stats2

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -64,7 +64,13 @@
                     <div>
                         
                         <h2 class="text-xl font-semibold text-gray-900">{{ $user->display_name }}</h2>
-                        <p class="text-gray-600">{{ $commonsUserInfo['registration']->format('d.m.Y') }} tarihinden beri üye</p>
+                        @if($commonsUserInfo && $commonsUserInfo['registration'])
+                            <p class="text-gray-600">{{ \Carbon\Carbon::parse($commonsUserInfo['registration'])->format('d.m.Y') }} tarihinden beri üye</p>
+                        @elseif($user->wikimedia_registration_date)
+                            <p class="text-gray-600">{{ $user->wikimedia_registration_date->format('d.m.Y') }} tarihinden beri üye</p>
+                        @else
+                            <p class="text-gray-600">{{ $user->email }}</p>
+                        @endif
                         @if($user->isWikimediaConnected())
                             <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
                                 Commons Bağlı
